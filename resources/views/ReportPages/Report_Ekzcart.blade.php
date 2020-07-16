fd33baed00bd8d4681e08fdf08872559462ee35c
Asata <55228825+Clunesate@users.noreply.github.com>
2020-07-16 13:16:20 +0300

    add ekzkart

--- resources/views/ReportPages/Report_Ekzcart.blade.php
+++ resources/views/ReportPages/Report_Ekzcart.blade.php
@@ -18,19 +18,28 @@
   .tfio td:last-child {
     padding-left: 10mm;
   }
+  .tekz td, th {
+    padding: 1.3mm;
+  }
  </style>
  <style type="text/css" media="print">
   @page {
    size: auto;
    margin: 0;
   }
+  .photo{
+   width: 3cm;
+   height: 4cm;
+   min-width:3cm;
+   min-height:4cm;
+  }
  </style>
 </head>
 <body class="body-report">
  <div class="wrap-list">
   <div class="row">
-   <div class="col" style="width: 50%; float: left; height: 200mm;">
-     <div class="row" style="padding-left:12mm; padding-top: 5mm;">Экзаменационный лист № 18783</div>
+   <div class="col" style="width: 50%; float: left;">
+     <div class="row" style="padding-left:12mm; padding-top: 8mm;">Экзаменационный лист № 18783</div>
      <div class="row" style="padding-left:12mm; padding-top: 2mm;">Форма обучения: Очная</div>
      <div class="row" style="padding-left:12mm; padding-top: 2mm;">Государственное и муниципальное управление
                                                                   (Государственное и региональное управление)
@@ -52,13 +61,15 @@
       </table>
      </div>
    </div>
-   <div class="col" style="float: right; height: 200mm; width: 50%;">
+   <div class="col" style="float: right; width: 50%;">
      <div class="col" style="float: left;">
-      <div class="photo" style="margin-top: 5mm; line-height:4cm; border: 1px solid black; width: 3cm; height: 4cm; text-align: center;">
+      <div class="photo" style="margin-top: 8mm; line-height:4cm;
+      border: 1px solid black; width: 3cm; height: 4cm; text-align: center;
+      min-width: 3cm; min-height: 4cm;">
        Фото</div>
      </div>
      <div class="col">
-      <div class="row" style="padding-top: 5mm; padding-left: 35mm;">Дата выдачи</div>
+      <div class="row" style="padding-top: 8mm; padding-left: 35mm;">Дата выдачи</div>
       <div class="row" style="padding-left: 35mm; padding-top: 2mm;">
       "<span style="border-bottom: 1px solid black;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span>"
        <span style="border-bottom: 1px solid black;">
@@ -67,8 +78,131 @@
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
        </span> 2020г.
      </div>
+     <div class="row" style="max-width: 50mm; padding-left: 35mm; padding-top: 3mm;">
+      Ответственный секретарь
+      приемной комиссии
+     </div>
+     <div class="row" style="padding-left: 35mm; padding-top: 4mm;">
+      <span style="border-bottom: 1px solid black;">
+       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      </span>
+     </div>
+     <div class="row" style="padding-left: 45mm; padding-top: 1mm; font-size: 12px;">
+      (подпись)
+     </div>
+     <div class="row" style="padding-top: 5mm; padding-left: 35mm;">Печать</div>
      </div>
    </div>
+  </div>
+  <div class="row" style="text-align: center;">
+   &nbsp;
+  </div>
+  <div class="row" style="text-align: center;">
+   <h3>I. Оценки за вступительные экзамены</h3>
+  </div>
+  <div class="row">
+   <table class="tekz" border="1" cellspacing="0" style="margin: 5mm 15mm 5mm 15mm;">
+    <thead>
+     <tr>
+      <th>№</br>п/п</th>
+      <th>Название учебного предмета</th>
+      <th>Характер экзамена</th>
+      <th>Дата экзамена</th>
+      <th colspan="2">Оценка</th>
+      <th>Фамилии и инициалы экзаменаторов</th>
+      <th>Подписи экзаменаторов</th>
+     </tr>
+     <tr>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th>Цифрами</th>
+      <th>Прописью</th>
+      <th></th>
+      <th></th>
+     </tr>
+    </thead>
+    <tbody>
+     <tr>
+      <td>1.</td>
+      <td colspan="3" style="text-align: center;">Средний балл документа об образовании</td>
+      <td style="text-align: center;">94,78</td>
+      <td></td>
+      <td></td>
+      <td></td>
+     </tr>
+     <tr>
+      <td>2.</td>
+      <td>Обществознание</td>
+      <td></td>
+      <td></td>
+      <td style="text-align: center;">48</td>
+      <td></td>
+      <td></td>
+      <td></td>
+     </tr>
+     <tr>
+      <td>3.</td>
+      <td>Русский язык (тест)</td>
+      <td></td>
+      <td></td>
+      <td style="text-align: center;">46</td>
+      <td></td>
+      <td></td>
+      <td></td>
+     </tr>
+    </tbody>
+   </table>
+  </div>
+  <div class="row" style="padding-left:12mm; padding-top: 5mm;">
+   <h3 style="float:left; margin: 0;">II. Сумма баллов за вступительные экзамены</h3>
+   <span style="border-bottom: 1px solid black; margin-left: 2mm;">
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;
+   </span>
+  </div>
+  <div class="row" style="font-size: 12px;">
+   <span style="margin-left: 18mm;">(цифрами)</span>
+   <span style="margin-left: 60mm;">(прописью)</span>
+  </div>
+  <div class="row" style="padding-left: 80mm; padding-top: 5mm;">
+   <h4 style="float:left; margin: 0;">Ответственный секретарь</br>приемной комиссии</h4>
+  </br>
+   <span style="border-bottom: 1px solid black; margin-left: 2mm;">
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   </span>
+  <span style="margin-left: 3mm;">И.В. Хорошевская</span>
   </div>
  </div>
 </body>
