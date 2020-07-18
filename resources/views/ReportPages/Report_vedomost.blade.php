89b3dc5fb05055dd2d135256684a4047b12b11e4
Asata <55228825+Clunesate@users.noreply.github.com>
2020-07-18 16:22:22 +0300

    fix report_vedomost
    1) Change font-size
    2) add new paddings
    3) normalize table and page footer

--- resources/views/ReportPages/Report_vedomost.blade.php
+++ resources/views/ReportPages/Report_vedomost.blade.php
@@ -28,34 +28,38 @@
 			.row{
 				width: 100%;
 			}
+			.ztable td{
+					padding: 14px;
+					font-size: 16px;
+			}
+			.ztable th{
+					font-size: 19px;
+			}
    		</style>
 	</head>
 	<body class="body-report">
 		<div class="wrap-list" id="wrap-list">
-			<div class="list-header">
-				<h2>ГОУ ВПО ЛНР «ЛУГАНСКИЙ НАЦИОНАЛЬНЫЙ</br>
+			<div class="list-header" style="margin-left: 77px;">
+				<h2 style="font-size: 22px;">ГОУ ВПО ЛНР «ЛУГАНСКИЙ НАЦИОНАЛЬНЫЙ</br>
 				УНИВЕРСИТЕТ ИМЕНИ ТАРАСА ШЕВЧЕНКО »</h2>
 			</div>
-			<div class="row" style="text-align:center;">Форма обучения: <? echo $ved_info->fo_name == 'Очная' ? '<u>очная</u>, заочная' : 'очная, <u>заочная</u>' ?></div>
-			<div class="row" style="text-align:center; font-size: 12px;">(подчеркнуть)</div>
-			<div class="row" style="text-align:center; font-size: 16px;"><h3>ВЕДОМОСТЬ ВСТУПИТЕЛЬНОГО ЭКЗАМЕНА</h3></div>
-			<div class="row" style="padding-left: 20mm;">
+			<div class="row" style="text-align:center; font-size: 20px; margin-left: 60px;">Форма обучения: <? echo $ved_info->fo_name == 'Очная' ? '<u>очная</u>, заочная' : 'очная, <u>заочная</u>' ?></div>
+			<div class="row" style="text-align:center; font-size: 14px;margin-left: 130px;">(подчеркнуть)</div>
+			<div class="row" style="text-align:center; font-size: 20px; margin-left: 77px;"><h3>ВЕДОМОСТЬ ВСТУПИТЕЛЬНОГО ЭКЗАМЕНА</h3></div>
+			<div class="row" style="padding-left: 32mm; font-size: 18px;">
 				Уровень профессионального образования
 				<!-- Если будешь тянуть сюда данные из БД убери &nbsp, если данные приходят пустые, то вставляй  &nbsp -->
-				<div style="border-bottom: 1px solid black; width: 90mm;float:right;  margin-right: 44mm; text-align:center;">{{ $ved_info->st_name }}</div>
+				<div style="font-size: 18px; border-bottom: 1px solid black; width: 90mm;float:right;  margin-right: 35mm; text-align:center;">{{ $ved_info->st_name }}</div>
 			</div>
-			<div class="row" style="text-align:center; margin-left: 34mm; margin-top: 2mm; font-size: 12px;">(бакалавриат, бакалавриат на основе СПО, магистратура)</div>
-			<div class="row" style="max-width: 166mm; margin-left: 20mm;border-bottom: 1px solid black; margin-top: 2mm; text-align:center;">{{ $ved_info->te_name }}</div>
-			<div class="row" style="text-align:center; font-size: 12px;">(форма вступительного экзамена)</div>
-			<div class="row" style="max-width: 166mm; margin-left: 20mm;border-bottom: 1px solid black; margin-top: 5mm;">{{ $ved_info->predmet_name }}</div>
-			<div class="row" style="text-align:center; font-size: 12px;">(название экзамена)</div>
-			<div class="row" style="padding-left: 20mm; margin-top: 6mm;">
-				Направление подготовки (специальность)
-				<!-- Если будешь тянуть сюда данные из БД убери &nbsp, если данные приходят пустые, то вставляй  &nbsp -->
-				<div style="border-bottom: 1px solid black; width: 90mm;float:right;  margin-right: 44mm;">{{ '('.$ved_info->minid.') '.$ved_info->group_name }}</div>
-			</div>
+			<div class="row" style="text-align:center; margin-left: 114mm; margin-top: 2mm; font-size: 14px; width: auto;">(бакалавриат, бакалавриат на основе СПО, магистратура)</div>
+			<div class="row" style="max-width: 166mm;margin-left: 42mm; font-size: 18px; border-bottom: 1px solid black; margin-top: 5mm; text-align:center;">{{ $ved_info->te_name }}</div>
+			<div class="row" style="text-align:center; font-size: 14px; margin-left: 20mm;">(форма вступительного экзамена)</div>
+			<div class="row" style="font-size: 18px; max-width: 176mm; margin-left: 32mm;border-bottom: 1px solid black; margin-top: 5mm;">{{ $ved_info->predmet_name }}</div>
+			<div class="row" style="text-align:center; font-size: 14px; margin-left: 18mm;">(название экзамена)</div>
 
-			<div class="row" style="padding-left: 20mm; margin-top: 6mm;">
+<div class="row" style="border-bottom: 1px solid black;max-width: 176mm; font-size: 18px; margin-top: 5mm; margin-left: 32mm;">{{ '('.$ved_info->minid.') '.$ved_info->group_name }}</div>
+<div class="row" style="text-align:center; font-size: 14px;margin-left: 78px;">	Направление подготовки (специальность)</div>
+			<div class="row" style="padding-left: 32mm; margin-top: 5mm; font-size: 18px;">
 				Дата проведения вступительного экзамена
 				<div style="float:right; margin-right: 50mm;">
 					«<span style="border-bottom:1px solid black;">
@@ -70,12 +74,12 @@
 				</div>
 			</div>
 
-			<div class="row" style="padding-left: 20mm; margin-top: 3mm;">
+			<div class="row" style="padding-left: 32mm; margin-top: 5mm; font-size: 18px;">
 				Фамилии и инициалы экзаменаторов
 				<!-- Если будешь тянуть сюда данные из БД убери &nbsp, если данные приходят пустые, то вставляй  &nbsp -->
 				<div style="border-bottom: 1px solid black; width: 90mm;float:right;  margin-right: 44mm;"> &nbsp &nbsp &nbsp</div>
 			</div>
-			<table border="1" cellspacing="0" style="margin-left: 20mm; margin-top: 3mm; width: 166mm; max-width: 166mm;">
+			<table class="ztable"border="1" cellspacing="0" style="margin-left: 20mm; margin-top: 3mm; width: 204mm; max-width: 204mm;">
 				<thead>
 					<tr>
 						<th rowspan="2">№ </br>п/п</th>
@@ -96,7 +100,7 @@
 							<td>{{ $ved->famil.' '.$ved->name.' '.$ved->otch }}</td>
 							<td style="text-align:center;">{{ $ved->ball }}</td>
 							<td>{{ $text_ball[$ved->id] }}</td>
-						</tr>	
+						</tr>
 					@endforeach
 					@for ($i = count($vedomost) + 1; $i <= 30; $i++)
 						<tr>
@@ -109,15 +113,15 @@
 					@endfor
 				</tbody>
 			</table>
-			<div class="row" style="margin-left: 20mm; margin-top: 3mm;">
+			<div class="row" style="margin-left: 32mm; margin-top: 12mm; font-size: 18px;">
 				Ответственный секретарь </br>
 				приёмной комиссии
 				<div style="float:right; margin-right: 45mm;">И. В. Хорошевская</div>
 			</div>
 			@if($ved_info->st_id == 1) <div class="row" style="margin-left: 20mm; margin-top: 10mm; max-width: 162mm;">Председатель предметной комиссии</div>
-			@else<div class="row" style="margin-left: 20mm; margin-top: 3mm; max-width: 162mm;">Председатель профессиональной аттестационной комиссии</div>
+			@else<div class="row" style="margin-left: 32mm; margin-top: 5mm; max-width: 162mm; font-size: 18px;">Председатель профессиональной аттестационной комиссии</div>
 			@endif
-			<div class="row" style="margin-left: 20mm; margin-top: 3mm; max-width: 162mm;">
+			<div class="row" style="margin-left: 32mm; margin-top: 5mm; max-width: 162mm;">
 				<span style="border-bottom:1px solid black">
 					&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
 					&nbsp&nbsp&nbsp&nbsp
@@ -127,15 +131,16 @@
 					&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
 					&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
 					&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+					&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
 				</span>
 			</div>
-			<div class="row" style="margin-left: 22mm; font-size: 12px;">
+			<div class="row" style="margin-left: 33mm; font-size: 14px; font-size: 14px;">
 				(подпись)
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
 				(фамилия, инициалы)
 			</div>
-			<div class="row" style="margin-left: 20mm; margin-top: 7mm;">Экзаменаторы:</div>
-			<div class="row" style="margin-left: 20mm; margin-top: 3mm; max-width: 162mm;">
+			<div class="row" style="margin-left: 32mm; font-size: 18px; margin-top: 5mm;">Экзаменаторы:</div>
+			<div class="row" style="margin-left: 32mm; margin-top: 5mm; max-width: 162mm;">
 				<span style="border-bottom:1px solid black">
 					&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
 					&nbsp&nbsp&nbsp&nbsp
@@ -145,15 +150,16 @@
 					&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
 					&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
 					&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+					&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
 				</span>
 			</div>
-			<div class="row" style="margin-left: 22mm; font-size: 12px;">
+			<div class="row" style="margin-left: 33mm; font-size: 14px;">
 				(подпись)
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
 				(фамилия, инициалы)
 			</div>
 
-			<div class="row" style="margin-left: 20mm; margin-top: 3mm; max-width: 162mm;">
+			<div class="row" style="margin-left: 32mm; margin-top: 3mm; max-width: 162mm;">
 				<span style="border-bottom:1px solid black">
 					&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
 					&nbsp&nbsp&nbsp&nbsp
@@ -163,10 +169,11 @@
 					&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
 					&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
 					&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+					&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
 				</span>
 			</div>
 
-			<div class="row" style="margin-left: 22mm; font-size: 12px;">
+			<div class="row" style="margin-left: 33mm; font-size: 14px;">
 				(подпись)
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
 				(фамилия, инициалы)
