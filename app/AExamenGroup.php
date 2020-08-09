d15fdf7f7ad205184625a71404288c0b123c0620
A-Div <small.ball.92@gmail.com>
2020-08-09 15:32:42 +0300

    added grafikExam

---
+++ app/AExamenGroup.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AExamenGroup extends Model
+{
+	protected $table = 'abit_examenGroup';
+    public $timestamps = false;
+    
+}
---
+++ app/PersTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PersTest extends Model
+{
+	protected $table = 'pers_tests';
+	public $timestamps = false;
+
+}
